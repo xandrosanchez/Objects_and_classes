3544785b868ff7391cadc98e673776f400c9b520
Alexander Baklushin <din.myrk@gmail.com>
2022-08-07 12:40:53 +0300

    готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author john = new Author("John","John");
+        Book bluesky = new Book("bluesky", john, 1999);
+        Author sasha = new Author("Sasha","Sasha");
+        Book blacksky = new Book("blacksky",sasha,2000);
+        blacksky.setPublication_year(2001);
+        Book[] arr = new Book[] {blacksky,bluesky,null,null,null};
+        Book brownsky = new Book("brownsky",john,2005);
+        addBook(brownsky,arr);
+        printBooks(arr);
+    }
+
+    public static void addBook(Book book, Book arr[]){
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] == null){
+                arr[i] = book;
+                break;
+            }
+            i++;
+        }
+    }
+
+    public static void printBooks(Book arr[]){
+        int i = 0;
+        while (i < arr.length){
+            if (arr[i] != null){
+                System.out.println(arr[i].getName() + ":" + arr[i].getTitle() + ":" + arr[i].getPublication_year());
+            }
+            i++;
+        }
     }
 }
